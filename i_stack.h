5da2123685992aa286cd3aecbd57f3c2ad694a47
Junun0402 <95282048+Junun0402@users.noreply.github.com>
2021-11-30 19:38:09 +0900

    Add files via upload

---
+++ i_stack.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#pragma once
+#define MAX_SIZE 10
+
+class i_Arraystack {
+	private:
+		int top;
+		int data[MAX_SIZE];
+	public:
+		i_Arraystack() {
+			top =-1;
+		}
+		bool isEmpty(void);
+		bool isfull(void);
+		void push(int e);
+		int pop(void);
+};
